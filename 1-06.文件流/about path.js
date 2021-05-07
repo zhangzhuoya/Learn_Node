d61e5897450e1f86ee9eee2a6528155d6602f757
zhangzhuoya <1114976368@qq.com>
2021-05-07 17:24:32 +0800

    feat:初始化master

--- 1-06.文件流/about path.js
+++
@@ -1,3 +0,0 @@
-const URL = require('url');
-const url = new URL.URL("https://www.jd.com/?cu=true&utm_source");
-console.log(url);
--- 1-06.文件流/文件目录信息.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require("path");
-const filename = path.resolve(__dirname,"./myfiles/");
-async function test() {
-    const pathes = await fs.promises.readdir(filename);
-    console.log(pathes);
-    
-}
-test()
